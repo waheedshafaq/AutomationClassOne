425285dc3b1c1c47eaedc17fd0c1939b1ac0f912
abdulwaheedshafaq <abdulwaheedshafaq@abduls-air.lan>
2020-02-02 16:46:57 -0500

    First_Push_Feb_02

---
+++ AutomationClassOne/src/main/java/com/DataTypePackage/DataType.java
@@ -0,0 +1,99 @@
+package com.DataTypePackage;
+
+public class DataType {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//integer datatype
+		int x = 1;
+		int y = 2;
+		int j = 3;
+		x = 5;
+		
+		
+		// double datatype
+		double a = 1.5;
+		a = 2.1;
+		
+		double d = 2.5;
+		double h = 2;
+		
+		System.out.println(d*h);
+	
+		
+		
+		
+		
+		//character datatype. it can only hold in 1 character and its limited
+		
+		char z = 'z';
+		
+//		string is a non-primitive datatype, its a class
+		
+		String name = "waheed";
+		
+//		Boolean Datatype, only has two values true or false
+		
+		boolean b = true;
+		boolean c = false;
+		
+		
+//		println: is a method that takes parameters, it takes arguments
+		
+//		System.out.println("Hello World");
+		
+//		System.out.println(2*3);
+//		System.out.println(y*3);
+//		System.out.println(j*y);
+//		System.out.println(j*j);
+		
+//		System.out.println(b);
+//		System.out.println(a*d);
+		
+//		System.out.println("My name is " + name);
+		
+//		System.out.println("My Name is" + " "+ name);
+//		System.out.println(h); the value is 2 but now it should show 2.0.
+		
+		
+//		creating method
+		
+		subtruction (){
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+
+
+	}
+
+	
+public static void addition() {
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+}
